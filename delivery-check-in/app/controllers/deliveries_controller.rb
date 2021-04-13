5057705719a8b9bba71f992ff91b4d5b607369c1
RL2020 <loc.fitness1@gmail.com>
2021-04-13 15:08:58 -0400

    cleaned deliveries views, added deliveries form, made site links

--- delivery-check-in/app/controllers/deliveries_controller.rb
+++ delivery-check-in/app/controllers/deliveries_controller.rb
@@ -5,33 +5,13 @@ class DeliveriesController < ApplicationController
     erb :"/deliveries/index.html"
   end
 
-  # GET: /deliveries/new
-  get "/deliveries/new" do
-    erb :"/deliveries/new.html"
-  end
-
+  
   # POST: /deliveries
   post "/deliveries" do
-    redirect "/deliveries"
+    redirect "/complete"
   end
-
-  # GET: /deliveries/5
-  get "/deliveries/:id" do
+   
+  get '/complete' do
     erb :"/deliveries/show.html"
-  end
-
-  # GET: /deliveries/5/edit
-  get "/deliveries/:id/edit" do
-    erb :"/deliveries/edit.html"
-  end
-
-  # PATCH: /deliveries/5
-  patch "/deliveries/:id" do
-    redirect "/deliveries/:id"
-  end
-
-  # DELETE: /deliveries/5/delete
-  delete "/deliveries/:id/delete" do
-    redirect "/deliveries"
-  end
+  end 
 end
